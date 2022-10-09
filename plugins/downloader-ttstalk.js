c1df90e50f3bb56b21b2535f3825672f1a568096
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-10-09 20:37:22 +0700

    test ehem

---
+++ plugins/downloader-ttstalk.js
@@ -0,0 +1,23 @@
+//by Ekuzika
+import axios from 'axios'
+
+
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+  axios.get('http://m.caliph.my.id/api/tiktokuser.php?usr=' + text)
+  .then((res) => {
+          let resnya = res.data.result
+          let imgny = resnya.user.avatarLarger
+          let hasil = `*> Username:* ${resnya.user.uniqueId}\n*> Fullname:* ${resnya.user.nickname}\n*> Id:* ${resnya.user.id}\n*> Verified:* ${resnya.user.verified}\n*> Private:* ${resnya.user.privateAccount}\n*> Followers:* ${resnya.stats.followerCount}\n*> Following:* ${resnya.stats.followingCount}\n*> Bio:* ${resnya.user.signature}\n`
+conn.sendButton(m.chat, hasil, author, imgny, [
+    ['Menu', `.? all`], 
+], m)
+    })
+        .catch(_ => m.reply('Server RestApi sedang error!'))
+}
+
+
+handler.help = ['tiktokstalk', 'ttstalk'].map(v => v + ' <username>')
+handler.tags = ['internet']
+handler.command = /^(tiktok|tt)stalk$/i
+
+export default handler
