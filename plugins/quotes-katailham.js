437ab7d43985e4be9dd41c11427cfc7ed722c5cd
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-10-09 14:07:12 +0700

    Create quotes-katailham.js

---
+++ plugins/quotes-katailham.js
@@ -0,0 +1,21 @@
+//by Ekuzika
+import axios from 'axios'
+
+
+let handler = async(m, { conn, usedPrefix, command }) => {
+  axios.get('https://betabotz-api.herokuapp.com/api/random/katailham?apikey=BetaBotz')
+  .then((res) => {
+          let hasil = `${res.hasil}`
+conn.sendButton(m.chat, hasil, author, [
+    ['Next', `${usedPrefix+command}`], 
+], m)
+    })
+        .catch(_ => m.reply('Server RestApi sedang error!'))
+}
+
+
+handler.help = ['katailham']
+handler.tags = ['quotes']
+handler.command = /^(katailham)$/i
+
+export default handler
