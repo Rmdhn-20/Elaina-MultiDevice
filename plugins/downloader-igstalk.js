36b8b1adefb3c766df4effba76a37b6ef7c9ac47
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-10-10 09:55:02 +0700

    oek

--- plugins/downloader-igstalk.js
+++ plugins/downloader-igstalk.js
@@ -1,38 +1,22 @@
-//https://github.com/Rlxfly/re-md
-import { instagramStalk } from '@bochilteam/scraper'
+//https://github.com/Rlxfly/re-md (error)
+// recode by ekuzika
+
+
+import axios from 'axios'
 
 let handler= async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `Example use ${usedPrefix}${command} <username>`
-    const {
-        username,
-      avatar,
-        name,
-        description,
-        followersH,
-        followingH,
-        postsH,
-    } = await instagramStalk(args[0])
-
-  let data = `
-💌 ${username} » 「 ${name} 」
-
-🎏 ${followersH}  ғᴏʟʟᴏᴡᴇʀs
-🎀 ${followingH}  ғᴏʟʟᴏᴡɪɴɢ
-📝 ${postsH} Pᴏꜱᴛ
-📑 Bɪᴏ: ${description}
-`.trim()
-
-  let pp = await( await conn.getFile(avatar)).data
-  
-  conn.sendHydrated(m.chat, '「  sᴛᴀʟᴋɪɴɢ 」' , data, pp, `https://instagram.com/${username.replace(/^@/, '')}`, 'Link Profile', '0', 'Nothing', [
-
-      [null],
-
-      [null],
-
-      [null, null]
-
-    ], null,  { asLocation: true })
+    axios.get(`https://betabotz-api.herokuapp.com/api/stalk/ig?username=${args[1]}&apikey=BetaBotz`)
+    .then((res) => {
+        let hasilnya = res.data.result
+        let sendny = `\n\n*> Username:* ${hasilnya.username}\n*> Fullname:* ${hasilnya.fullName}\n*> Followers:* ${hasilnya.followers}\n*> Following:* ${hasilnya.following}\n*> Post:* ${hasilnya.postsCount}\n*> Bio:* ${hasilnya.bio}\n\n`
+        let pp = hasilnya.profilePicHD
+        
+        conn.sendButton(m.chat, sendny, author, pp, [
+    ['Menu', `.? all`], 
+], m)
+    })
+        .catch(_ => m.reply('Server RestApi sedang error!'))
 }
 
 handler.help = ['igstalk'].map(v => v + ' <username>')
