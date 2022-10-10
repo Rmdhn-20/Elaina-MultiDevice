b79014db57e849e5bfc9ef75c10d16485aa6ff44
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-10-10 16:15:02 +0700

    Update downloader-ptl.js

--- plugins/downloader-ptl.js
+++ plugins/downloader-ptl.js
@@ -4,12 +4,9 @@ import fetch from 'node-fetch'
 
 
 let handler = async(m, { conn, usedPrefix, command }) => {
-  conn.reply(m.chat, 'Proses...', ftrol)
-  let res = `https://ziy.herokuapp.com/api/asupan2k?apikey=xZiyy`
-  conn.sendButton(m.chat, `Nih kak...`, author, res, [
-    ['Menu', `.? all`],
-    ['Next', `${usedPrefix+command}`], 
-], m)
+  conn.reply(m.chat, 'Proses...')
+  let res = await getBuffer(`https://ziy.herokuapp.com/api/asupan2k?apikey=xZiyy`)
+  conn.sendButtonVid(m.chat, res, `Nih kak...`, wm, 'Next', `${usedPrefix+command}`, m)
 }
 
 
@@ -18,3 +15,24 @@ handler.tags = ['asupan']
 handler.command = /^(ptl)$/i
 
 export default handler
+
+const getBuffer = async (url, options) => {
+	try {
+		options ? options : {}
+		const res = await axios({
+			method: "get",
+			url,
+			headers: {
+				'DNT': 1,
+                    'User-Agent': 'GoogleBot',
+				'Upgrade-Insecure-Request': 1
+			},
+			...options,
+			responseType: 'arraybuffer'
+		})
+		return res.data
+	} catch (e) {
+		console.log(`Error : ${e}`)
+	}
+}
+
