b2edca22a246c9c97dd97680f0a12d44271a0b96
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-10-10 16:41:43 +0700

    Update downloader-asupanbocil.js

--- plugins/downloader-asupanbocil.js
+++ plugins/downloader-asupanbocil.js
@@ -1,16 +1,13 @@
 // By Ekuzika
 
 import fetch from 'node-fetch'
+import axios from 'axios'
 
 
 let handler = async(m, { conn, usedPrefix, command }) => {
-  conn.reply(m.chat, 'Proses...', ftrol)
-  let res = await fetch(`https://ziy.herokuapp.com/api/asupan/bocil?apikey=xZiyy`)
-  let vid = res.result.url
-  conn.sendButton(m.chat, `Nih kak...`, author, vid, [
-    ['Menu', `.? all`],
-    ['Next', `${usedPrefix+command}`], 
-], m)
+  conn.reply(m.chat, 'Proses...')
+  let res = await getBuffer(`https://betabotz-api.herokuapp.com/api/asupan/bocil?apikey=BetaBotz`)
+  conn.sendButtonVid(m.chat, res, `Nih kak...`, wm, 'Next', `${usedPrefix+command}`, m)
 }
 
 
@@ -19,3 +16,24 @@ handler.tags = ['asupan']
 handler.command = /^(asupanbocil|bocil)$/i
 
 export default handler
+
+const getBuffer = async (url, options) => {
+	try {
+		options ? options : {}
+		const res = await axios({
+			method: "get",
+			url,
+			headers: {
+				'DNT': 1,
+                    'User-Agent': 'GoogleBot',
+				'Upgrade-Insecure-Request': 1
+			},
+			...options,
+			responseType: 'arraybuffer'
+		})
+		return res.data
+	} catch (e) {
+		console.log(`Error : ${e}`)
+	}
+}
+
