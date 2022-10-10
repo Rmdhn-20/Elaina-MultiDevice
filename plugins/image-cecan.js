7c2fa79646f849677de6a53c84bc9d9305228fb8
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-10-10 11:28:27 +0700

    Create image-cecan.js

---
+++ plugins/image-cecan.js
@@ -0,0 +1,20 @@
+// By Ekuzika
+
+import fetch from 'node-fetch'
+
+
+let handler = async(m, { conn, usedPrefix, command }) => {
+  conn.reply(m.chat, 'Proses...', ftrol)
+  let res = `https://ziy.herokuapp.com/api/cecan2k?apikey=xZiyy`
+  conn.sendButton(m.chat, `Nih kak...`, author, res, [
+    ['Menu', `.? all`],
+    ['Next', `${usedPrefix+command}`], 
+], m)
+}
+
+
+handler.help = ['cecan']
+handler.tags = ['internet']
+handler.command = /^(cecan)$/i
+
+export default handler
