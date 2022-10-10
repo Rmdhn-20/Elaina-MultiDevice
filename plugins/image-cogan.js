1c82184017d0dd6ebe92584815f066a9ee9ce8d1
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-10-10 11:23:04 +0700

    Create image-cogan.js

---
+++ plugins/image-cogan.js
@@ -0,0 +1,20 @@
+// By Ekuzika
+
+import fetch from 'node-fetch'
+
+
+let handler = async(m, { conn, usedPrefix, command }) => {
+  conn.reply(m.chat, 'Proses...', ftrol)
+  let res = `https://betabotz-api.herokuapp.com/api/wallpaper/cogan?apikey=BetaBotz`
+  conn.sendButton(m.chat, `Nih kak...`, author, res, [
+    ['Menu', `.? all`],
+    ['Next', `${usedPrefix+command}`], 
+], m)
+}
+
+
+handler.help = ['cogan']
+handler.tags = ['internet']
+handler.command = /^(cogan)$/i
+
+export default handler
