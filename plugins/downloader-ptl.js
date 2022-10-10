2c10c7ea6a7af61be4681e0067bbae2aa85967cc
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-10-10 12:00:53 +0700

    Create downloader-ptl.js

---
+++ plugins/downloader-ptl.js
@@ -0,0 +1,20 @@
+// By Ekuzika
+
+import fetch from 'node-fetch'
+
+
+let handler = async(m, { conn, usedPrefix, command }) => {
+  conn.reply(m.chat, 'Proses...', ftrol)
+  let res = `https://ziy.herokuapp.com/api/asupan2k?apikey=xZiyy`
+  conn.sendButton(m.chat, `Nih kak...`, author, res, [
+    ['Menu', `.? all`],
+    ['Next', `${usedPrefix+command}`], 
+], m)
+}
+
+
+handler.help = ['ptl']
+handler.tags = ['asupan']
+handler.command = /^(asupan)$/i
+
+export default handler
