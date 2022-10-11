235c74d64d01a02b0c42857b09c3d0568f8adf5a
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-10-11 18:32:14 +0700

    coba

--- plugins/youtube-ytv.js
+++ plugins/youtube-ytv.js
@@ -3,15 +3,14 @@ import fetch from 'node-fetch'
 import { servers, ytv } from '../lib/y2mate.js'
 let handler = async(m, { conn, args, isPrems, isOwner }) => {
     if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
-    let chat = global.db.data.chats[m.chat]
-    let server = (args[1] || servers[0]).toLowerCase()
-    let { dl_link, thumb, title, filesize, filesizeF } = await ytv(args[0], servers.includes(server) ? server : servers[0])
-    let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize
-    conn.reply(isLimit ? `Ukuran File: ${filesizeF}\nUkuran file diatas ${limit} MB, download sendiri: ${dl_link}` : global.wait, m)
-    let _thumb = {}
-    try { _thumb = { thumbnail: await (await fetch(thumb)).buffer() } } catch (e) {}
+	let restnya = await fetchJson(`https://api-xcoders.site/api/download/ytmp4?url=${args[0]}&apikey=cyXNcMnw3x`)
+	let { title, channel_name, published_at, viewers, likes, comments, quality, size, duration, thumbnail, url } = restnya.result
+	let tiny = await fetchJson(`https://api-xcoders.site/api/tools/tinyurl?url=${url}&apikey=cyXNcMnw3x`)
+	let shorturl = tiny.result
+    let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < size
+    conn.reply(isLimit ? `Ukuran File: ${size}\nUkuran file diatas ${limit}MB, silahkan download sendiri: ${shorturl}` : global.wait, m)
     m.reply(wait)
-    if (!isLimit) await conn.sendButtonVid(m.chat, dl_link, `*Title:* ${title}\n*Filesize:* ${filesizeF}`.trim(), wm, 'menu', '.?', m)
+    if (!isLimit) await conn.sendButtonVid(m.chat, url, `*Title:* ${title}\n*Filesize:* ${size}\n*Duration:* ${duration}\n*Quality:* ${quality}\n*Viewers:* ${viewers}\n*Likes:* ${likes}`.trim(), wm, 'menu', '.?', m)
 	//await conn.sendMessage(m.chat, { document: { url: dl_link }, mimetype: 'video/mp4', fileName: title + `.mp4`}, {quoted: m})
 //conn.sendFile(m.chat, dl_link, title + '.mp4', `
 //*Title:* ${title}
@@ -23,3 +22,38 @@ handler.tags = ['downloader']
 handler.command = /^yt(v(idi?e?o)?|mp4)?$/i
 
 export default andler
+
+
+const fetchJson = (url, options) => new Promise(async (resolve, reject) => {
+    fetch(url, options)
+        .then(response => response.json())
+        .then(json => {
+            // console.log(json)
+            resolve(json)
+        })
+        .catch((err) => {
+            reject(err)
+        })
+})
+
+
+const getBuffer = async (url, options) => {
+	try {
+		options ? options : {}
+		const res = await axios({
+			method: "get",
+			url,
+			headers: {
+				'DNT': 1,
+                    'User-Agent': 'GoogleBot',
+				'Upgrade-Insecure-Request': 1
+			},
+			...options,
+			responseType: 'arraybuffer'
+		})
+		return res.data
+	} catch (e) {
+		console.log(`Error : ${e}`)
+	}
+}
+
