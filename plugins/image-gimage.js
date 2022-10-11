6bd0ce63a2634fe1ff763bebdbf4c5d9c7b40aa4
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-10-11 12:11:59 +0700

    hha

--- plugins/image-gimage.js
+++ plugins/image-gimage.js
@@ -1,17 +1,24 @@
+// Fix By Ekuzika
+
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (!text) throw `Use example ${usedPrefix}${command} Minecraft`
     const res = await googleImage(text)
     let image = res.getRandom()
     let link = image
-    conn.sendHydrated(m.chat,`
+    conn.sendButton(m.chat, `
 *${htki} ɢᴏᴏɢʟᴇ ɪᴍᴀɢᴇ ${htka}*
 🔎 *Result:* ${text}
 🌎 *Source:* Google
-`, wm, link, link, '🔗 URL', null, null, [['Next', `.image ${text}`],[null,null],[null,null]],m)
+🔗 *Link:* ${link}
+`, wm2, link, [
+      ['Menu', `.? all`],
+      ['Next', `${usedPrefix+command} ${text}`],
+    ], m)
 }
 handler.help = ['gimage <query>', 'image <query>']
 handler.tags = ['internet']
 handler.command = /^(gimage|image)$/i
 
 export default handler
+
