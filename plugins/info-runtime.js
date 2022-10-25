56088115c4ecf5be9f77f096561035f685c6a6d9
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-10-25 10:33:16 +0700

    Hehe

--- plugins/info-runtime.js
+++ plugins/info-runtime.js
@@ -41,7 +41,7 @@ token: '9',
 curreyCode: 'IDR',
 totalCurrencyCode: '>〰<',
 totalAmount1000: '1000000',
-sellerJid: '6285736178354@s.whatsapp.net',
+sellerJid: `${global.nomorown}@s.whatsapp.net`,
 thumbnail: fs.readFileSync('./thumbnail.jpg')
 }}, {contextInfo: null, quoted: m})
 conn.relayWAMessage(prep)
