b93811b9ea83e75b97c11a435a73f954252d19bf
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-10-25 10:45:56 +0700

    Ok

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -13,50 +13,77 @@ let handler = async (m) => {
     else who = m.sender
     let user = global.db.data.users[who]
 let tqto = `*▸ - - - —「 BIG THANKS TO 」— - - - ◂*
+
 *❉ Adiwajshing:*
 https://github.com/adiwajshing
+
 *❉ Nurutomo:*
 https://github.com/Nurutomo
+
 *❉ Istikmal:* 
 https://github.com/BochilGaming
+
 *❉ Ariffb:*
 https://github.com/Ariffb25
+
 *❉ Ilman:*
 https://github.com/ilmanhdyt
+
 *❉ Amirul:*
 https://github.com/amiruldev20
+
 *❉ Rasel:*
 https://github.com/raselcomel
+
 *❉ Fatur:*
 https://github.com/Ftwrr
+
 *❉ Rominaru:*
 https://github.com/Rominaru
+
 *❉ Kannachann:*
 https://github.com/Kannachann
+
 *❉ The.sad.boy01:*
 https://github.com/kangsad01
+
 *❉ Ameliascrf:*
 https://github.com/Ameliascrf
+
 *❉ Fokus ID:*
 https://github.com/Fokusdotid
+
 *❉ Johannes:*
 https://github.com/Johannes2803
+
 *❉ BrunoSobrino:*
 https://github.com/BrunoSobrino
+
 *❉ Krisna:*
 https://github.com/NevtBotz
+
 *❉ LitRHap:*
 https://github.com/LitRHap
+
 *❉ Rlxfly:*
 https://github.com/Rlxfly
+
 *❉ Aine:*
 https://github.com/Aiinne
+
 *❉ Papah-Chan:*
 https://github.com/FahriAdison
-*▸ - - - —「 Penulis Ulang 」— - - - ◂*
+
 *❉ ImYanXiao:*
 https://github.com/ImYanXiao
+
+*▸ - - - —「 Penulis Ulang 」— - - - ◂*
+
+*❉ ${global.nameown}:*
+${global.sgh}
+
 *▸ - - - —「 Thanks For Suhu 」— - - - ◂*
+
 *❉ Kannachan:*
 *❉ The.sadboy.01:*
 *❉ Papah-Chan:*
@@ -66,10 +93,10 @@ https://github.com/ImYanXiao
 
 `
 conn.sendButtonDoc(m.chat, tqto, wm,'Thanks','Bilek', m, { contextInfo: { externalAdReply: { showAdAttribution: true,
-    mediaUrl: "https://Instagram.com/Xiao_yan_21",
+    mediaUrl: `${global.sig}`,
     mediaType: "VIDEO",
-    description: "https://Instagram.com/Xiao_yan_21", 
-    title: 'Elaina-MultiDevice',
+    description: `${global.sig}`, 
+    title: `${global.namebot}`,
     body: wm,
     thumbnail: thumb,
     sourceUrl: sgc
