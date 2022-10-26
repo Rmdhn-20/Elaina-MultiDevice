3553910e27372e5b94a83ad946ae6a672be3fb5b
Rmdhn-20 <76412415+Rmdhn-20@users.noreply.github.com>
2022-10-27 00:17:03 +0700

    auk ah

--- plugins/menu.js
+++ plugins/menu.js
@@ -607,10 +607,10 @@ const listMessage = {
                 },
             ]
         }
-      // await conn.sendMessage(m.chat, message, m, { mentionedJid: [m.sender] })
+       await conn.sendMessage(m.chat, message, m, { mentionedJid: [m.sender] })
         
         //MAIN MENU
-      conn.send2ButtonImg(m.chat, `${timeimg()}`, `*${ucapan()} ${name} 👋*\n`, text.trim(), 'SpeedTest', `${_p}speedtest`, 'Owner', `${_p}owner`, fkon, { contextInfo: { externalAdReply: { showAdAttribution: true,
+     /* conn.send2ButtonImg(m.chat, `${timeimg()}`, `*${ucapan()} ${name} 👋*\n`, text.trim(), 'SpeedTest', `${_p}speedtest`, 'Owner', `${_p}owner`, fkon, { contextInfo: { externalAdReply: { showAdAttribution: true,
     mediaUrl: sig,
     mediaType: 2,
     description: sig, 
@@ -620,7 +620,7 @@ const listMessage = {
     sourceUrl: sgh
 	}} 
 })
-
+*/
     //------------------- 2BUTTON VID
    // conn.sendMessage(m.chat, { video: { url: 'https://telegra.ph/file/c82d5c358495e8ef15916.mp4' }, gifPlayback: true, gifAttribution: ~~(Math.random() * 2), caption: text.trim(), footer: 'ᴍᴀᴅᴇ ᴡɪᴛʜ ❤ ʙʏ ɪᴍ-ʏᴀɴxɪᴀᴏ', templateButtons: [{ quickReplyButton: { displayText: 'Speedtest⚡', id: `${_p}speedtest` }}, { quickReplyButton: { displayText: 'Owner🎀', id: `${_p}owner` }} ] })
     
